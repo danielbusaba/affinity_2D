156fc8d233885c8dbe7bcff9b7fe8ee2e439217b
Daniel Busaba <DanielBusaba99@hotmail.com>
2019-05-22 17:56:41 -0400

    Added some error catching into file input

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,35 @@
-use std::fs;
-use std::env;
-use std::collections::HashMap;
+use std::fs;                    //Used for file I/O
+use std::env;                   //Used for command line arguments
+//use std::collections::HashMap;  //Imports HashMap data structure
 
-fn get_reuse_times(trace: &Vec<Vec<u64>>) -> HashMap<u64, u64>
+fn parse_file(filename: String, x: usize, y: usize) -> Vec<Vec<u64>>    //Converts a file of numbers seperated by spaces and new lines into a 2D array of those numbers
 {
-    let reuse_times: HashMap<u64, u64> = HashMap::new();
-    for i in 0 .. trace.len()
+    let contents = fs::read_to_string(&filename).expect(&("File Read Error: ".to_owned() + &filename)); //Reads the file into a String
+
+    let mut output = Vec::with_capacity(y); //Allocates the output row array
+    let mut rows = 0;   //Counts the number of rows
+    for token in contents.split('\n')   //Iterates over each line of the file
     {
-        for j in 0 .. trace.get(i).unwrap().len()
+        let mut inner = Vec::with_capacity(x);  //Allocates the column array
+        let mut cols = 0; //Counts the number of columns
+        for number in token.split(' ')    //Iterates over each number in the file
         {
-            print!("{} ", trace.get(i).unwrap().get(j).unwrap());
+            inner.push(number.parse::<u64>().unwrap_or_else(|_| panic!("Invalid Trace Element: {}", number)));  //Pushes casted number into the inner array
+            cols = cols + 1;
         }
-        println!();
+
+        if cols != x    //Checks to see if there were the correct number of columns
+        {
+            panic!("Trace is Not Rectangular or is of Wrong Dimension: {} != {}", cols, x);
+        }
+
+        output.push(inner); //Pushes the inner array into the outer array
+        rows = rows + 1;
     }
-    reuse_times
-}
 
-fn parse_file(filename: String) -> Vec<Vec<u64>>
-{
-    let contents = fs::read_to_string(&filename).expect(&("File Read Error: ".to_owned() + &filename));
-
-    let estimate = ((contents.len() as f64).sqrt() / 2.0) as usize;
-    let mut output = Vec::with_capacity(estimate);
-    for token in contents.split('\n')
+    if rows != y    //Checks to see if there were the correct number of rows
     {
-        let mut inner = Vec::with_capacity(estimate);
-        for item in token.split(' ')
-        {
-            inner.push(item.parse::<u64>().unwrap_or_else(|_| panic!("Invalid Trace Element: {}", item)));
-        }
-        output.push(inner);
+        panic!("Trace is Not Rectangular or is of Wrong Dimension: {} != {}", rows, y);
     }
 
     output
@@ -37,15 +37,14 @@ fn parse_file(filename: String) -> Vec<Vec<u64>>
 
 fn main()
 {
-    let v = vec!(vec!(0, 1, 2, 3), vec!(0, 1, 2, 3), vec!(0, 1, 2, 3), vec!(0, 1, 2, 3));
-    get_reuse_times(&v);
+    println!("{:?}", env::args());
 }
 
 #[test]
 fn test_file_input()
 {
     let expect = vec!(vec!(0, 1, 2, 3), vec!(0, 1, 2, 3), vec!(0, 1, 2, 3), vec!(0, 1, 2, 3));
-    let actual = parse_file(String::from("traces/simple_test_trace"));
+    let actual = parse_file(String::from("traces/simple_test_trace"), 4, 4);
     for i in 0 .. expect.len()
     {
         for j in 0 .. expect.get(i).unwrap().len()
