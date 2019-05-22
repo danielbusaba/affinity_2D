7e5c3ba029a7b91f33f155f1f6394645a73cdf02
Daniel Busaba <DanielBusaba99@hotmail.com>
2019-05-22 09:46:27 -0400

    Have 2D array declaration figured out along with basic unit testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+
+fn get_reuse_times(trace: &Vec<Vec<u64>>) -> HashMap<u64, u64>
+{
+    let reuse_times = HashMap::new();
+    for i in 0 .. trace.len()
+    {
+        for j in 0 .. trace.get(i).unwrap().len()
+        {
+            print!("{} ", trace.get(i).unwrap().get(j).unwrap());
+        }
+        println!();
+    }
+    reuse_times
+}
+
+fn main()
+{
+    let v = vec!(vec!(0, 1, 2, 3), vec!(0, 1, 2, 3), vec!(0, 1, 2, 3), vec!(0, 1, 2, 3));
+    get_reuse_times(&v);
+}
+
+#[test]
+fn test_reuse_times()
+{
+    assert_eq!(0, 0);
 }
