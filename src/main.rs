fd6321aedf3cba228ae4fb3c774cd0aa956fe638
Daniel Busaba <DanielBusaba99@hotmail.com>
2019-05-22 17:15:47 -0400

    Added file input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::fs;
+use std::env;
 use std::collections::HashMap;
 
 fn get_reuse_times(trace: &Vec<Vec<u64>>) -> HashMap<u64, u64>
 {
-    let reuse_times = HashMap::new();
+    let reuse_times: HashMap<u64, u64> = HashMap::new();
     for i in 0 .. trace.len()
     {
         for j in 0 .. trace.get(i).unwrap().len()
@@ -14,6 +16,25 @@ fn get_reuse_times(trace: &Vec<Vec<u64>>) -> HashMap<u64, u64>
     reuse_times
 }
 
+fn parse_file(filename: String) -> Vec<Vec<u64>>
+{
+    let contents = fs::read_to_string(&filename).expect(&("File Read Error: ".to_owned() + &filename));
+
+    let estimate = ((contents.len() as f64).sqrt() / 2.0) as usize;
+    let mut output = Vec::with_capacity(estimate);
+    for token in contents.split('\n')
+    {
+        let mut inner = Vec::with_capacity(estimate);
+        for item in token.split(' ')
+        {
+            inner.push(item.parse::<u64>().unwrap_or_else(|_| panic!("Invalid Trace Element: {}", item)));
+        }
+        output.push(inner);
+    }
+
+    output
+}
+
 fn main()
 {
     let v = vec!(vec!(0, 1, 2, 3), vec!(0, 1, 2, 3), vec!(0, 1, 2, 3), vec!(0, 1, 2, 3));
@@ -21,7 +42,15 @@ fn main()
 }
 
 #[test]
-fn test_reuse_times()
+fn test_file_input()
 {
-    assert_eq!(0, 0);
+    let expect = vec!(vec!(0, 1, 2, 3), vec!(0, 1, 2, 3), vec!(0, 1, 2, 3), vec!(0, 1, 2, 3));
+    let actual = parse_file(String::from("traces/simple_test_trace"));
+    for i in 0 .. expect.len()
+    {
+        for j in 0 .. expect.get(i).unwrap().len()
+        {
+            assert_eq!(expect.get(i).unwrap().get(j).unwrap(), actual.get(i).unwrap().get(j).unwrap());
+        }
+    }
 }
