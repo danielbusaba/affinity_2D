bf7f4c1596fb9fea6d9745187da2d954eee8a31c
Daniel Busaba <Daniel.Busaba@ibm.com>
2019-07-07 20:11:18 -0400

    Finished image processing

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,26 @@
 #![allow(unused_imports)]           //Removes annoying unused import warnings
 
 //Imports for code
+extern crate image;                 //Used for image processing
 use std::fs;                        //Used for file I/O
+use std::fs::File;                  //Used for writing to files
+use std::path::Path;                //Used for accessing files
 use std::env;                       //Used for command line arguments
 use std::collections::HashMap;      //Imports HashMap data structure
 use std::collections::HashSet;      //Imports HashMap data structure
+use image::ImageDecoder;            //Decodes images
+use image::GenericImageView;        //Gets image meta data
+use image::ImageBuffer;
+use image::Rgb;
 
 //Imports for tests
 extern crate rand;                  //Used for testing random cases
 use rand::{thread_rng, Rng};        //Used for random number generation
-use std::fs::File;                  //Used for writing to files
 use std::io::Write;                 //Used for writing to files
 
-const TRACE_DIR: &str = "traces/";    //Stores trace directory globally
+const TRACE_DIR: &str = "traces/";            //Stores trace directory globally
+const MAMMOGRAM_DIR: &str = "mammograms/";    //Stores trace directory globally
+const OUTPUT_DIR: &str = "output/";           //Stores output directory globally
 
 fn get_trace_file(filename: String, x: usize, y: usize) -> Vec<Vec<u64>>    //Converts a file of numbers seperated by spaces and new lines into a 2D array of those numbers
 {
@@ -189,7 +197,7 @@ fn get_frequencies_naive(trace: Vec<Vec<u64>>, window_sizes: Vec<usize>) -> (Vec
     (single_frequencies_list, joint_frequencies_list)
 }
 
-fn main()
+fn main() -> std::io::Result<()>
 {
     println!("{:?}", env::args());
     let arr = vec!(vec!(0, 1, 2, 3, 0),
@@ -197,19 +205,6 @@ fn main()
                    vec!(0, 1, 2, 3, 0),
                    vec!(0, 1, 2, 3, 0));
     let sizes = vec!(2, 3);
-    // let test = get_single_frequencies(arr.clone(), sizes.clone());
-
-    // let mut i = 0;
-    // for hash in test
-    // {
-    //     println!("Window Size {}", sizes[i]);
-    //     i = i + 1;
-
-    //     for key in hash.keys()
-    //     {
-    //         println!("{:?}: {}", key, hash.get(key).unwrap());
-    //     }
-    // }
 
     let naive = get_frequencies_naive(arr, sizes.clone());
     let mut i = 0;
@@ -235,6 +230,98 @@ fn main()
             println!("{:?}: {}", key, hash.get(key).unwrap());
         }
     }
+
+    for entry in fs::read_dir(MAMMOGRAM_DIR)?
+    {
+        let entry = entry?;
+        let img = image::open(entry.path()).unwrap().to_luma();
+        println!("Dimensions: {:?}", img.dimensions());
+        let mut image: image::GrayImage = image::ImageBuffer::new(img.width() - 2, img.height() - 2);
+        for i in 0 .. img.width() - 2
+        {
+            for j in 0 .. img.height() - 2
+            {
+                let mut square: Vec<Vec<u64>> = Vec::with_capacity(3);
+                let mut not_zero = false;
+                for r in i .. i + 3
+                {
+                    let mut col = Vec::with_capacity(3);
+                    for c in j .. j + 3
+                    {
+                        let num = img.get_pixel(r, c) [0];
+                        if num != 0
+                        {
+                            not_zero = true;
+                        }
+                        col.push(num as u64);
+                    }
+                    square.push(col);
+                }
+
+                if not_zero
+                {
+                    let frequencies = get_frequencies_naive(square, vec!(2));
+                    let mut max_diff: u8 = 0;
+                    let mut max_affinity: f64 = 0.0;
+                    for pair in frequencies.1 [0].keys()
+                    {
+                        let single_frequecy_a = *frequencies.0 [0].get(&pair.0).unwrap() as f64;
+                        let single_frequecy_b = *frequencies.0 [0].get(&pair.1).unwrap() as f64;
+                        let mut affinity = *frequencies.1 [0].get(pair).unwrap() as f64;
+                        if single_frequecy_b < single_frequecy_a
+                        {
+                            affinity = affinity / single_frequecy_b;
+                        }
+                        else
+                        {
+                            affinity = affinity / single_frequecy_a;
+                        }
+
+                        if affinity > max_affinity
+                        {
+                            max_affinity = affinity;
+                            if pair.0 > pair.1
+                            {
+                                max_diff = (pair.0 - pair.1) as u8;
+                            }
+                            else
+                            {
+                                max_diff = (pair.1 - pair.0) as u8;
+                            }
+                        }
+                        else if affinity == max_affinity
+                        {
+                            if pair.0 > pair.1
+                            {
+                                let diff = (pair.0 - pair.1) as u8;
+                                if diff > max_diff
+                                {
+                                    max_diff = (pair.0 - pair.1) as u8;
+                                }
+                            }
+                            else
+                            {
+                                let diff = (pair.1 - pair.0) as u8;
+                                if diff > max_diff
+                                {
+                                    max_diff = (pair.1 - pair.0) as u8;
+                                }
+                            }
+                        }
+                    }
+
+                    image.get_pixel_mut(i, j).data = [max_diff];
+                }
+                else
+                {
+                    image.get_pixel_mut(i, j).data = [0];
+                }
+            }
+        }
+        image.save(OUTPUT_DIR.to_owned() + &entry.file_name().into_string().unwrap()).unwrap();
+    }
+
+    Ok(())
 }
 
 #[test]
