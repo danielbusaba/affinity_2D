f70c6fb3e40110ac96b4490864b28442c76f89d5
Daniel Busaba <DanielBusaba99@hotmail.com>
2019-11-12 00:27:23 -0500

    Ran div16 analysis

--- sort.py
+++ sort.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 ANSWERS_FILE = "answers"
-DIRS = ["mammograms/", "output/", "saturated/", "output_max_diff/", "saturated_max_diff/", "output_center_diff/", "saturated_center_diff/", "output_average/", "saturated_average/"]
+DIRS = ["output/", "output_average/", "output_average_div16/", "output_center_diff/", "output_center_diff_div16/", "output_div16/", "output_max_diff/", "output_max_diff_div16/", "saturated/", "saturated_div16/", "saturated_output/", "saturated_output_average/", "saturated_output_average_div16/", "saturated_output_center_diff/", "saturated_output_center_diff_div16/", "saturated_output_div16/", "saturated_output_max_diff/", "saturated_output_max_diff_div16/", "base/", "base_div16/"]
 
 for dir in DIRS:
     if not os.path.exists(dir + "Normal"):
