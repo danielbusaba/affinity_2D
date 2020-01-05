3b481c0f9fb9f5c85fffef6c827c041925ffa5ed
Daniel Busaba <DanielBusaba99@hotmail.com>
2020-01-04 21:37:20 -0500

    Ran another round of testing, removed obsolete helper scripts

---
+++ tensorflow/retrain.sh
@@ -0,0 +1,13 @@
+declare -a folders=("output" "output_average" "output_average_div16" "output_center_diff" "output_center_diff_div16" "output_div16" "output_max_diff" "output_max_diff_div16" "saturated_base" "saturated_base_div16" "saturated_output" "saturated_output_average" "saturated_output_average_div16" "saturated_output_center_diff" "saturated_output_center_diff_div16" "saturated_output_div16" "saturated_output_max_diff" "saturated_output_max_diff_div16" "base" "base_div16")
+
+for i in "${folders[@]}"
+do
+    python3 -m scripts.retrain \
+            --bottleneck_dir=tf_files/bottlenecks \
+            --model_dir=tf_files/models \
+            --summaries_dir=tf_files/training_summaries \
+            --output_graph=tf_files/"$i"_graph.pb \
+            --output_labels=tf_files/"$i"_labels.txt \
+            --image_dir=../"$i" \
+            2>&1 | tee "$i".out
+done
